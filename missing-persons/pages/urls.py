282fd203946fa7b2936e37191b8b8a58154bf844
BZV83 <120076188+BZV83@users.noreply.github.com>
2023-03-29 22:33:01 -0600

    added routes

--- missing-persons/pages/urls.py
+++ missing-persons/pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import indexPageView
+from .views import aboutPageView
+from .views import faqPageView
 
 urlpatterns = [
-    path("", indexPageView, name = "index")
+    path("", indexPageView, name = "index"),
+    path("about/", aboutPageView, name = "about"),
+    path("faq/", faqPageView, name = "faq"),
 ]
