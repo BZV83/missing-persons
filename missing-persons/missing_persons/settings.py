4400b01e93f052ccef179531bdebe10a1d58c950
landonjg41 <120079186+landonjg41@users.noreply.github.com>
2023-04-11 20:51:27 -0600

    update settings.py
    .

--- missing-persons/missing_persons/settings.py
+++ missing-persons/missing_persons/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'missing_persons.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'person',
+        'USER' : 'postgres',
+        'PASSWORD' : 'test',
+        'HOST' : 'localhost',
     }
 }
 
