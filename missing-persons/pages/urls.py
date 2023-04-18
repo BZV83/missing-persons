e340f82ea4148442ff4117eab0361b51c0d112c2
landonjg41 <120079186+landonjg41@users.noreply.github.com>
2023-04-18 11:29:23 -0600

    update urls.py
    .

--- missing-persons/pages/urls.py
+++ missing-persons/pages/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from .views import indexPageView
 from .views import aboutPageView
 from .views import faqPageView
+from .views import showPeoplePageView
+from .views import displayPersonPageView
 
 urlpatterns = [
+    path("<int:id>/", displayPersonPageView, name= "displayPerson"),
+    path("showPeople/", showPeoplePageView, name ="showPeople"),
     path("", indexPageView, name = "index"),
-    path("about/", aboutPageView, name = "about"),
     path("faq/", faqPageView, name = "faq"),
+    path("about/", aboutPageView, name = "about"),
+   
 ]
