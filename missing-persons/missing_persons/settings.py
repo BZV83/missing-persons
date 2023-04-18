13e28893b8c13fe22a5c2a572eabf02aa353c6bb
BZV83 <120076188+BZV83@users.noreply.github.com>
2023-04-18 13:02:58 -0600

    Format display person page

--- missing-persons/missing_persons/settings.py
+++ missing-persons/missing_persons/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'person',
         'USER' : 'postgres',
-        'PASSWORD' : "test",
+        'PASSWORD' : 'test',
         'HOST' : 'localhost',
         'PORT' : 5432
     }
