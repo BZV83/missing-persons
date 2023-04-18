c3972291a3b0e7887f329162ce4f3c0a9aedbdeb
landonjg41 <120079186+landonjg41@users.noreply.github.com>
2023-04-18 11:30:22 -0600

    update views.py
    .

--- missing-persons/pages/views.py
+++ missing-persons/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
 
 # Create your views here.
 def indexPageView(request):
@@ -192,3 +193,24 @@ def aboutPageView(request):
 
 def faqPageView(request):
     return render(request, 'pages/faq.html')
+
+def showPeoplePageView(request) :
+
+    db_people = Person.objects.all() 
+
+    context = {
+            "data" : db_people
+    }
+
+    return render(request, 'pages/showPeople.html', context)   
+
+
+def displayPersonPageView(request, id) :
+
+    person = Person.objects.get(id=id)   
+
+    context = {
+            "data" : person
+    }
+
+    return render(request, 'pages/displayPerson.html', context)    
