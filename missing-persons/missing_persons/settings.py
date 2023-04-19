fe1e56f46dbad7f966ecbcd81462e467f672e29e
BZV83 <120076188+BZV83@users.noreply.github.com>
2023-04-18 19:09:26 -0600

    removed extra links

--- missing-persons/missing_persons/settings.py
+++ missing-persons/missing_persons/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'person',
         'USER' : 'postgres',
-        'PASSWORD' : 'test',
+        'PASSWORD' : "Id0n'ttakeLs",
         'HOST' : 'localhost',
         'PORT' : 5432
     }
