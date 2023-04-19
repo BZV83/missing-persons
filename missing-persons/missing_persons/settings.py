b05dbcf8376c2f2cb688df30d59808375cf50b75
BZV83 <120076188+BZV83@users.noreply.github.com>
2023-04-18 21:09:34 -0600

    changed password

--- missing-persons/missing_persons/settings.py
+++ missing-persons/missing_persons/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'person',
         'USER' : 'postgres',
-        'PASSWORD' : "Id0n'ttakeLs",
+        'PASSWORD' : '(private)',
         'HOST' : 'localhost',
         'PORT' : 5432
     }
